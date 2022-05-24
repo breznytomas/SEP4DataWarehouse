3f3a7b7e1ef0a188abececfda134e23d62f12c17
Tymon2115 <tymon.podlowski@gmail.com>
2022-05-24 22:31:59 +0200

    fixed date converson issues

--- SEP4DataWarehouse/Services/DwServices/Implementations/DimHumidityService.cs
+++ SEP4DataWarehouse/Services/DwServices/Implementations/DimHumidityService.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SEP4DataWarehouse.Contexts.DbContext;
 using SEP4DataWarehouse.Contexts.DwContext;
 using SEP4DataWarehouse.Models.DwModels;
@@ -6,33 +7,21 @@ using SEP4DataWarehouse.Services.DwServices.Interfaces;
 
 namespace SEP4DataWarehouse.Services.DwServices.Implementations;
 
-public class DimHumidityService : IDimHumidity
-{
+public class DimHumidityService : IDimHumidity {
     private readonly GreenHouseDwContext _Dwcontext;
 
 
-    public DimHumidityService(GreenHouseDwContext dwcontext)
-    {
+    public DimHumidityService(GreenHouseDwContext dwcontext) {
         _Dwcontext = dwcontext;
     }
-    
-    public async Task<float> GetHumidityAverage(string boardId, DateTime timeFrom, DateTime timeTo)
-    {
-        try
-        {
-            /*var humidity = (from hum in _Dwcontext.Dimhumidities
-                    join factHum in _Dwcontext.Factmeasurements
-                        on hum.HId equals factHum.HId
-                    orderby hum.HId
-                    select new
-                    {
-                        hum.HId,
-                        dateTime = DateTime.Parse(hum.MeasureDate.ToString()),
-                        factHum.Humidityvalue,
-                    }
-                ).Where(query => query.dateTime > timeFrom && query.dateTime < timeTo).Average(value=> value.Humidityvalue);*/
 
+    public async Task<float> GetHumidityAverage(string boardId, DateTime timeFrom, DateTime timeTo) {
 
+        var from = Int32.Parse(timeFrom.ToString("yyyyMMdd"));
+        var to = Int32.Parse(timeTo.ToString("yyyyMMdd"));
+       
+        
+        try {
             var humidity = (from hum in _Dwcontext.Dimhumidities
                     join factHum in _Dwcontext.Factmeasurements
                         on hum.HId equals factHum.HId
@@ -40,31 +29,20 @@ public class DimHumidityService : IDimHumidity
                     select new
                     {
                         hum.HId,
+                        dateTime = hum.MeasureDate, 
                         factHum.Humidityvalue,
                     }
-                ).Average(average => average.Humidityvalue);
-
-           
-           
-
+                ).Where(h => h.dateTime >= from && h.dateTime <= to).Average(h=> h.Humidityvalue);
+            
+            return humidity ?? -999;
 
 
-            //DateTime = DateTime.ParseExact(Convert.ToString(hum.MeasureDate),"YYYYmmdd",null)
-
-
-
-            return (float) humidity;
-
-
-
-
-            /*var board = await _context.Boards.Include(b =>b.HumidityList ).FirstAsync(board => board.Id.Equals(boardId));
-            //todo by tomas null pointer possible reference below has to be addressed better 
+            /*var board = await _context.Boards.Include(b =>b.HumidityList ).FirstAsync(board => board.Id.Equals(boardId));gi
+            //todo by tomas null pointer possible reference below has to be addressed better (I have made simple if ~ tymon)
             if (board.HumidityList != null) return board.HumidityList;
             throw new Exception();*/
         }
-        catch (Exception e)
-        {
+        catch (Exception e) {
             Console.WriteLine(e);
             throw new Exception();
         }
